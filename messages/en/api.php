da02b94de6741c79a39f22e8c8db0acfdde61e16
Dmitry Zabreika <=>
2024-02-16 12:39:51 +0300

    CRNC-0000 [BE] - Add translations

---
+++ messages/en/api.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'API_ERROR_-1' => 'Internal Error: Please contact support team',
+    'API_ERROR_2' => 'Invalid Request Data',
+    'API_ERROR_3' => 'Entity was not saved: %s',
+    'API_ERROR_4' => 'Currency not found',
+    'API_ERROR_5' => 'Invalid amount'
+];
