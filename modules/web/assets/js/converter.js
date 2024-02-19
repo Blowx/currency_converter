da2adf111b7ad8a9fda6169b001d4c1b00d9b6c2
Dmitry Zabreika <=>
2024-02-19 10:36:49 +0300

    CRNC-0011 [BE] Enhances: Add timeout to converter.js for better user experience

--- modules/web/assets/js/converter.js
+++ modules/web/assets/js/converter.js
@@ -1,30 +1,36 @@
-$( document ).ready( function () {
-    $( 'input[name^="currency-name-"]' ).on( 'input', function () {
-        var amount = $( this ).val();
-        var type = $( this ).data( 'code' );
+$(document).ready(function () {
+    var timeout;
 
-        $.ajax( {
-            url: '/api/v1/currency/' + type + '/exchange',
-            type: 'GET',
-            data: {amount: amount},
-            success: function ( response ) {
-                console.log( response );
-                updateInputs( response.items );
-            },
-            error: function ( error ) {
-                console.error( 'Error:', error );
-            }
-        } );
-    } );
+    $('input[name^="currency-name-"]').on('input', function () {
+        clearTimeout(timeout);
 
-    function updateInputs( items ) {
-        items.forEach( function ( item ) {
+        timeout = setTimeout(function () {
+            var amount = $(this).val();
+            var type = $(this).data('code');
+
+            $.ajax({
+                url: '/api/v1/currency/' + type + '/exchange',
+                type: 'GET',
+                data: {amount: amount},
+                success: function (response) {
+                    console.log(response);
+                    updateInputs(response.items);
+                },
+                error: function (error) {
+                    console.error('Error:', error);
+                }
+            });
+        }.bind(this), 500);
+    });
+
+    function updateInputs(items) {
+        items.forEach(function (item) {
             var inputName = 'currency-name-' + item.code;
-            var input = $( 'input[name="' + inputName + '"]' );
+            var input = $('input[name="' + inputName + '"]');
 
-            if ( input.length > 0 ) {
-                input.val( item.amount );
+            if (input.length > 0) {
+                input.val(item.amount);
             }
-        } );
+        });
     }
-} );
+});
